5037b218706d52fee822f24514dac65e176860b3
Denis Onder <denis_onder@protonmail.com>
2020-08-06 22:39:24 +0200

    Slowly porting over the components from the original portfolio

---
+++ components/Navigation.js
@@ -0,0 +1,81 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import setCurrentlyActiveIcon from '../../events/setCurrentlyActiveIcon';
+import redirect from '../../helpers/redirect';
+import ResponsiveNavigation from './imports/ResponsiveNavigation';
+import NavigationModal from './imports/NavigationModal';
+import '../../scss/navigation.scss';
+
+export default function Navigation() {
+  return (
+    <Fragment>
+      <nav id="navigation">
+        {/* Logo */}
+        <div className="navigation_logo">
+          <p className="temp_logo">
+            d<span>o</span>
+          </p>
+        </div>
+        {/* Navigation Icons */}
+        <ul className="navigation_icons">
+          <Link to="/">
+            <li
+              id="/"
+              className="navigation_icons_icon navigation_icons_icon--link"
+              onClick={() => setCurrentlyActiveIcon('/')}
+            >
+              <i className="fas fa-home"></i>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li
+              id="/about"
+              className="navigation_icons_icon navigation_icons_icon--link"
+              onClick={() => setCurrentlyActiveIcon('/about')}
+            >
+              <i className="fas fa-user"></i>
+            </li>
+          </Link>
+          <Link to="/portfolio">
+            <li
+              id="/portfolio"
+              className="navigation_icons_icon navigation_icons_icon--link"
+              onClick={() => setCurrentlyActiveIcon('/portfolio')}
+            >
+              <i className="fas fa-laptop-code"></i>
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li
+              id="/contact"
+              className="navigation_icons_icon navigation_icons_icon--link"
+              onClick={() => setCurrentlyActiveIcon('/contact')}
+            >
+              <i className="fas fa-envelope"></i>
+            </li>
+          </Link>
+        </ul>
+        {/* Social Media Icons */}
+        <ul className="navigation_icons">
+          <li
+            className="navigation_icons_icon navigation_icons_icon--github"
+            data-link="https://github.com/denis-onder"
+            data-text="GitHub"
+            onClick={redirect}
+          >
+            <i className="fab fa-github"></i>
+          </li>
+          <li
+            className="navigation_icons_icon navigation_icons_icon--linkedin"
+            onClick={redirect}
+            data-link="https://linkedin.com/in/denis-onder"
+          >
+            <i className="fab fa-linkedin"></i>
+          </li>
+        </ul>
+      </nav>
+      <ResponsiveNavigation />
+      <NavigationModal />
+    </Fragment>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,67 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Particles from 'react-particles-js';
+import styles from '../styles/Home.module.scss';
+
+const particlesConfig = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 1500,
+      },
+    },
+    line_linked: {
+      enable: true,
+      opacity: 0.02,
+    },
+    move: {
+      direction: 'right',
+      speed: 0.05,
+    },
+    size: {
+      value: 1,
+    },
+    opacity: {
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.05,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'grab',
+      },
+    },
+    modes: {
+      grab: {
+        distance: 75,
+      },
+    },
+  },
+  retina_detect: true,
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.home}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>denis onder - home</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Particles params={particlesConfig} />
+      <h1 className={styles.home_name}>
+        denis <span>onder</span>
+      </h1>
+      <div className="home_text">
+        <p>I am a full-stack web developer from Bosnia and Herzegovina.</p>
+        <p>Currently working within the MERN stack.</p>
+      </div>
+      <Link href="/contact">contact me</Link>
     </div>
   );
 }
